7ef95b820c79f437700ae1fb8db5672115aa4291
Miguel Chan <miguel.chanc1991@gmail.com>
2022-11-13 16:25:06 -0800

    Setup commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * This is the initial point of our project.
+ * We want to build a really small CLI App that will display the Chords/Notes/Scales for a given
+ * Key in the Circle of Fifths.
+ * 
+ * There are two modules:
+ * 
+ * NoteFinder: which will return the corret Scale/Chords/Notes to use
+ * NotePlayer: Which will play a set of Notes.
+ */
